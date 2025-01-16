26eac537abde0c2ea7640c61defecc02014995be
sumut <umut.soysal@tomdigital.com>
2025-01-16 09:18:09 +0300

    Fix dashboard delete

--- app/src/main/java/com/skyvo/mobile/top/words/dashboard/DashboardUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.skyvo.mobile.top.words.dashboard
-
-import com.skyvo.mobile.core.base.manager.Language
-import com.skyvo.mobile.core.base.viewmodel.UIState
-
-data class DashboardUIState(
-    val nativeLanguage: Language? = null,
-    val learnLanguage: Language? = null
-) : UIState 
